5db2051a174aa9180d6f91f43515e02829d05e41
Dapnek <ahmaddaffaamd@gmail.com>
2024-07-30 01:33:42 +0700

    Minor Fix

--- articles/models.py
+++ articles/models.py
@@ -6,11 +6,6 @@ class Article(models.Model):
     title = models.CharField(max_length=200)
     short_description = models.CharField(max_length=500)
     description = models.TextField()
-    photo1 = models.ImageField(upload_to='articles/')
-    photo2 = models.ImageField(upload_to='articles/', blank=True, null=True)
-    photo3 = models.ImageField(upload_to='articles/', blank=True, null=True)
-    photo4 = models.ImageField(upload_to='articles/', blank=True, null=True)
-    photo5 = models.ImageField(upload_to='articles/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- articles/urls.py
+++ articles/urls.py
@@ -3,5 +3,5 @@ from .views import article_list, article
 
 urlpatterns = [
     path('', article_list, name='article_list'),
-    path('/article', article, name='article'),
+    path('article', article, name='article'),
 ]
